1a6cb028af2cf905fd6a085a4a9683c7c96e4e84
TheLuckyMojo <ethanmorales0730@gmail.com>
2023-01-24 21:05:50 -0800

    4.14 - Destructuring arguments

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
 // Stateless Functional Component
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className='navbar bg-body-tertiary'>
       <a className='navbar-brand' href='#'>
         Navbar{" "}
         <span className='badge badge-pill badge-secondary'>
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
