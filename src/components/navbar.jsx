64ba88e26a459c07a1a8ccd45fcade5978cb26dc
TheLuckyMojo <ethanmorales0730@gmail.com>
2023-01-24 20:58:03 -0800

    4.12 - Lifting up the state

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,11 +4,12 @@ class NavBar extends Component {
   render() {
     return (
       <nav className='navbar bg-body-tertiary'>
-        <div className='container-fluid'>
-          <a className='navbar-brand' href='#'>
-            Navbar
-          </a>
-        </div>
+        <a className='navbar-brand' href='#'>
+          Navbar{" "}
+          <span className='badge badge-pill badge-secondary'>
+            {this.props.totalCounters}
+          </span>
+        </a>
       </nav>
     );
   }
